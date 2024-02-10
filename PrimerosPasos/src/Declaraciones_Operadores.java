791e4afb3c8380a9e656ec6a80e7d2a695143f40
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2024-02-10 14:22:51 +0100

    Add files via upload

--- PrimerosPasos/src/Declaraciones_Operadores.java
+++ PrimerosPasos/src/Declaraciones_Operadores.java
@@ -4,16 +4,19 @@ public class Declaraciones_Operadores {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a=5;
 		
-		int b;
+		
+		double a=5;
+		
+		double b;
 		
 		b=7;
 		
-		int c=b+a;
+		double c=b/a;
+		
+		//c-=6;
 		
 		System.out.println(c);
-		
 		
 	}
 
